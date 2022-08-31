32d3ec0bf204bb0c4df8706085c2899a21c6921d
Hellek-Greed <nimnarr@gmail.com>
2022-08-31 10:12:34 +0300

    task 1 added

---
+++ homework_lession_1/task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите целое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int number_2 = int.Parse(Console.ReadLine());
+
+if (number_1 > number_2)
+{
+        Console.WriteLine($"Число {number_1} больше числа {number_2}.");
+}
+
+if (number_1 < number_2)
+{
+        Console.WriteLine($"Число {number_2} больше числа {number_1}.");
+}
+
+if (number_1 == number_2)
+{
+        Console.WriteLine($"Числа {number_1} и {number_2} равны.");
+}
