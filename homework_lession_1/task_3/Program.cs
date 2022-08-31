79753c55f4752b10916620413f27b42b3ab7a649
Hellek-Greed <nimnarr@gmail.com>
2022-08-31 10:16:06 +0300

    task 3 added

---
+++ homework_lession_1/task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+        Console.WriteLine($"Число {number} является четным.");
+}
+else
+{
+        Console.WriteLine($"Число {number} не является четным.");
+}
