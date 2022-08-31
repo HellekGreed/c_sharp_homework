9ecb953a3cda863bb04e99954009f1ca94a4052c
Hellek-Greed <nimnarr@gmail.com>
2022-08-31 10:15:58 +0300

    task 2 added

---
+++ homework_lession_1/task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите первое целое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int number_2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье целое число: ");
+int number_3 = int.Parse(Console.ReadLine());
+
+if (number_1 == number_2 & number_1 == number_3)
+{
+        Console.WriteLine("Все три числа равны.");
+}
+
+else
+{
+    if (number_1 >= number_2)
+    {
+            number_2 = number_1;
+    }
+    if (number_2 >= number_3)
+    {
+            number_3 = number_2;
+    }
+    Console.WriteLine($"Большее из 3-ех чисел, число {number_3}.");
+}
