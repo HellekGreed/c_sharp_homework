50f835cf7e42b026b30f4e5b9bd2a3077f715a67
Hellek-Greed <nimnarr@gmail.com>
2022-09-03 15:21:08 +0300

    Task 1 added

---
+++ homework_lession_2/task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿void SecondDigit(int number)
+{
+        int result = (number / 10) % 10; 
+        Console.WriteLine($"Второй разряд числа {number} - {result}");
+}
+
+Console.Write("Введите целое, трехзначное число: ");
+int digit = int.Parse(Console.ReadLine());
+
+SecondDigit(digit);
+
