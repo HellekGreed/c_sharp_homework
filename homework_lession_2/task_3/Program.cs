94bcdb93266d97282fab5cb0809620ac7043782c
Hellek-Greed <nimnarr@gmail.com>
2022-09-03 15:44:14 +0300

    Task 3 added

---
+++ homework_lession_2/task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿void Weekend(int day)
+{
+        if(day == 6 | day == 7)
+        {
+                Console.WriteLine($"День недели под номером {day} является выходным.");
+        }
+        else
+        {
+                Console.WriteLine($"День недели под номером {day} не является выходным.");
+        }
+}
+Console.Write("Введите целое число от 1 до 7: ");
+int number = int.Parse(Console.ReadLine());
+
+Weekend(number);
+
