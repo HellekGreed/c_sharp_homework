b6b4a14a70838469ebba6346931ba049ce12efac
Hellek-Greed <nimnarr@gmail.com>
2022-09-03 15:35:23 +0300

    Task 2 added

---
+++ homework_lession_2/task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿void ThirthDigit(int number)
+{
+        if(number > 99)
+        {
+                int digit = number;
+                while(digit > 1000)
+                {
+                        digit /= 10;
+                }
+                digit %= 10;
+                Console.WriteLine($"Третья цифра числа {number} - {digit}");
+        }
+
+        else
+        {
+            Console.WriteLine($"Число {number} не имеет третьей цифры");
+        }
+}
+
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+
+ThirthDigit(number);
+
