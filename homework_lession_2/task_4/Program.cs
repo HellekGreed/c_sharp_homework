cb89fa1f1e8000d09d3da4866bd225f68a0fca51
Hellek-Greed <nimnarr@gmail.com>
2022-09-03 15:46:30 +0300

    Task 4 added

---
+++ homework_lession_2/task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿void Glo()
+{
+        int x = new Random().Next(0, 2000000000);
+        Console.WriteLine(x);
+        int x2 = x;
+        int count = 1;
+        while (x2 > 100)
+        {
+                x2 = x2 / 10;
+                count = count * 10;
+        }
+        int result = ((x2 / 10) * count) + (x % count);
+        Console.WriteLine(result);
+}
+
+Glo();
+
