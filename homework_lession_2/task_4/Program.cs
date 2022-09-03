cc374890ed3cb86cd446a131f2515cdd215107bc
Hellek-Greed <nimnarr@gmail.com>
2022-09-03 16:05:16 +0300

    Task 4 fix

--- homework_lession_2/task_4/Program.cs
+++ homework_lession_2/task_4/Program.cs
@@ -1,16 +1,22 @@
 ﻿void Glo()
 {
-        int x = new Random().Next(100, 2000000000);
-        Console.WriteLine(x);
+        int x = new Random().Next(1, 2000000000);
         int x2 = x;
         int count = 1;
-        while (x2 > 100)
+        if(x2 > 9)
         {
+            while (x2 > 100)
+            {
                 x2 = x2 / 10;
                 count = count * 10;
+            }
+            int result = ((x2 / 10) * count) + (x % count);
+            Console.WriteLine($"Число {x} без второй цифры с лева : {result}");
         }
-        int result = ((x2 / 10) * count) + (x % count);
-        Console.WriteLine(result);
+        else
+        {
+                Console.WriteLine($"В числе {x} нет второй цифры");
+        }
 }
 
 Glo();
