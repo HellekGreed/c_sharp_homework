8a1ca7148efb9ab98a5fe9c7476ae689f5f48827
Hellek-Greed <nimnarr@gmail.com>
2022-09-07 13:52:36 +0300

    Task 3 added

---
+++ homework_lession_3/task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿double[] MakeCubeOfNumber(int number)
+{
+        double[] cubeNumbers = new double[number];
+
+        for(int i = 0; i < number; i++)
+        {
+                cubeNumbers[i] = Math.Pow(i + 1, 3);
+        }
+
+        return cubeNumbers;
+}
+
+
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+
+double[] result = MakeCubeOfNumber(n);
+
+for (int i = 0; i < result.Length; i++)
+{
+        Console.WriteLine(result[i]);
+}
+
+
+
