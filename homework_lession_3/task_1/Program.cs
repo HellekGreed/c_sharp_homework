d01f578cae1928f7b07fea18973d2de7053c1167
Hellek-Greed <nimnarr@gmail.com>
2022-09-07 13:00:34 +0300

    Task 1 added

---
+++ homework_lession_3/task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿string Reverse(int num)
+{        
+    int num1 = Math.Abs(num);
+    string num2 = null;
+    
+    while(num1 > 0)
+    {
+        string n = Convert.ToString(num1 % 10);     
+        num2 += n;
+        num1 /= 10;
+       
+    }
+
+    return num2;
+}
+
+Console.Write("Введите целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Reverse(number1));
+
+if (Math.Abs(number1) == number2)
+{
+    Console.WriteLine($"Число {number1} является палиндромом.");
+}
+else
+{
+    Console.WriteLine($"Число {number1} не является палиндромом.");
+}
+
