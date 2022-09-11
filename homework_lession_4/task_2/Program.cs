0256e75e13645c41e974535ba896f0e77839dded
Hellek-Greed <nimnarr@gmail.com>
2022-09-11 20:10:29 +0300

    Task 2 added

---
+++ homework_lession_4/task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿int SummDigitNumber(int number)
+{
+        int tempNumber = Math.Abs(number);
+        int result = 0;
+        while(tempNumber > 0)
+        {
+                result += tempNumber % 10;
+                tempNumber /= 10;
+        }
+        return result;
+}
+
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = SummDigitNumber(number);
+
+Console.WriteLine($"Сумма цифр в числе {number} равняется {result}");
