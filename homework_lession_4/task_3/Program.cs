cf91aaf7c6cfadc0b0257f54575c27d5c3d80257
Hellek-Greed <nimnarr@gmail.com>
2022-09-11 20:26:45 +0300

    Task 3 added

---
+++ homework_lession_4/task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿void ShowArray(int[] array)
+{
+        for(int i = 0; i < array.Length; i++)
+        {
+                Console.Write($"{array[i]}, ");
+        }
+}
+
+
+int[] array = new int[8];
+
+Console.WriteLine("Введите целые числа(после введения каждого числа нижмите Enter)");
+for(int i = 0; i < array.Length; i++)
+{
+        array[i] = int.Parse(Console.ReadLine());
+}
+
+Console.Write("Введенные вами числа: ");
+ShowArray(array);
+
