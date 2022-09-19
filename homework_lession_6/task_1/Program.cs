b58383edc57ff3f90ff4262d477f841bcc8c847f
Hellek-Greed <nimnarr@gmail.com>
2022-09-19 10:06:27 +0300

    Task 1 added

---
+++ homework_lession_6/task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] GetArray()
+{  
+    Console.Write("Введите колличество чисел которые содираетесь ввести(Это должно быть целое число): ");
+    int size = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+    int[] array = new int[size];
+
+    Console.WriteLine("Введите целые числа(после введение числа нажмите Enter): ");
+
+    for( int i = 0;i < size; i++ )
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return array;
+
+}
+
+int CountPositive(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) { count++; }
+    }
+    return count;
+}
+
+int[] array = GetArray();
+
+int count = CountPositive(array);
+
+Console.WriteLine($"Вы ввели {count} положительных чисел.");
+
