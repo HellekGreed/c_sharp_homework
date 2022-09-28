ec449bf17819e8cbe260feab820ec40092f464cc
Hellek-Greed <nimnarr@gmail.com>
2022-09-28 09:16:19 +0300

    Task 2 added

---
+++ homework_lession_9/task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿int GetSum(int m, int n)
+{
+        if(m == n)
+        {
+                return n;
+        }
+
+        else if(m < n)
+        {
+                return m + GetSum(m + 1, n);
+        }
+
+        else
+        {
+                return m + GetSum(m - 1, n);
+        }
+}
+
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int res = GetSum(m, n);
+
+Console.Write($"Сумма ввсе натуральных чисел от {m} до {n}: {res}");
+
