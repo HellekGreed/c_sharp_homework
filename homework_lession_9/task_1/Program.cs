2254adb3fa068e5701654304a7cccd411ce73a65
Hellek-Greed <nimnarr@gmail.com>
2022-09-28 09:16:12 +0300

    Task 1 added

---
+++ homework_lession_9/task_1/Program.cs
@@ -0,0 +1,30 @@
+﻿string PrintNumbers(int m, int n)
+{
+        string res = String.Empty;
+
+        if(n == m)
+        {
+                return Convert.ToString(n);
+        }
+        else if(m < n)
+        {
+                res += m + " " + PrintNumbers(m + 1, n);
+                return res;
+        }
+        else
+        {
+                res += m + " " + PrintNumbers(m - 1, n);
+                return res;
+        }
+}
+Console.Write("Введите М: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write($"Натуральные числа от {m} до {n}: ");
+Console.WriteLine(PrintNumbers(m, n));
+
