525a9829c37757bc2cb475b91984080d750a83ed
Hellek-Greed <nimnarr@gmail.com>
2022-09-28 09:16:25 +0300

    Task 3 added

---
+++ homework_lession_9/task_3/Program.cs
@@ -0,0 +1,29 @@
+﻿int Akkerman(int m, int n)
+{
+        if(m == 0)
+        {
+                return n + 1;
+        }
+
+        else if(m != 0 && n == 0)
+        {
+                return Akkerman(m - 1, 1);
+        }
+
+        else
+        {
+                return Akkerman(m - 1, Akkerman(m, n - 1));
+        }
+}
+Console.Write("Введите М: ");
+int m = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine();
+
+Console.Write("Введите N: ");
+int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine();
+
+int result = Akkerman(m, n);
+
+Console.Write($"A({m},{n}) = {result}");
+
