f87fc89d7c052ce7b57655436592bfe28e75811c
Hellek-Greed <nimnarr@gmail.com>
2022-10-02 13:24:38 +0300

    Program.cs fig for incapsulation

--- homework_snake/Program.cs
+++ homework_snake/Program.cs
@@ -10,11 +10,15 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Console.Clear();
+
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
+
+            Console.ReadLine();
         }
     }
 }
